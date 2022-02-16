7fac34050db6a116e9964aaf98684c50fc05c2d3
Zubairalam07 <99797761+Zubairalam07@users.noreply.github.com>
2022-02-16 14:47:56 +0500

    Update calculator-spyder.py

--- calculator-spyder.py
+++ calculator-spyder.py
@@ -1,54 +1,66 @@
-# Program make a simple calculator
 
-# This function adds two numbers
+
+   # Program make a simple calculator that can add, subtract, multiply and divide using functions
+
+# define functions
 def add(x, y):
-    return x + y
+   """This function adds two numbers"""
 
-# This function subtracts two numbers
+   return x + y
+
 def subtract(x, y):
-    return x - y
+   """This function subtracts two numbers"""
 
-# This function multiplies two numbers
+   return x - y
+
 def multiply(x, y):
-    return x * y
+   """This function multiplies two numbers"""
 
-# This function divides two numbers
+   return x * y
+
 def divide(x, y):
-    return x / y
+   """This function divides two numbers"""
 
+   return x / y
 
+# take input from the user
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
 
-while True:
-    # take input from the user
-    choice = input("Enter choice(1/2/3/4): ")
+choice = input("Enter choice(1/2/3/4):")
 
-    # check if choice is one of the four options
-    if choice in ('1', '2', '3', '4'):
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
 
-        if choice == '1':
-            print(num1, "+", num2, "=", add(num1, num2))
+if choice == '1':
+   print(num1,"+",num2,"=", add(num1,num2))
 
-        elif choice == '2':
-            print(num1, "-", num2, "=", subtract(num1, num2))
+elif choice == '2':
+   print(num1,"-",num2,"=", subtract(num1,num2))
 
-        elif choice == '3':
-            print(num1, "*", num2, "=", multiply(num1, num2))
+elif choice == '3':
+   print(num1,"*",num2,"=", multiply(num1,num2))
 
-        elif choice == '4':
-            print(num1, "/", num2, "=", divide(num1, num2))
-        
-        # check if user wants another calculation
-        # break the while loop if answer is no
-        next_calculation = input("by Dilshad Ahmad. Let's do next calculation? (yes/no): ")
-        if next_calculation == "no":
-          break
+elif choice == '4':
+   print(num1,"/",num2,"=", divide(num1,num2))
+else:
+   print("Invalid input")
+
+
+
+   
+
     
-    else:
-        print("Invalid Input")
+
+
+ 
+        
+        
+        
+
+    
+
+ 
